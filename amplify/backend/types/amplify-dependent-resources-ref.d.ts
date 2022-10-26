2ae6daf7fe3fe7f2c2875bcea97ab168f5a760a6
Mac in Astral <hugolam922@gmail.com>
2022-10-25 18:26:55 -0700

    first init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,22 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifytodo64e6ac83": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "AdminGroupRole": "string"
+        }
+    },
+    "api": {
+        "amplifytodo": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
